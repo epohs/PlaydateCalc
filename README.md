43a299cbe7b44c1ed36967f3e2cc3a1d2aca18a2
epohs <blurryphoto@gmail.com>
2022-03-06 21:30:13 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PlayDate Calculator
+
+## A calculator for [Panic](https://panic.com)'s [PlayDate game console](https://play.date).
---
+++ source/main.lua
@@ -0,0 +1,35 @@
+
+
+import "CoreLibs/graphics"
+import "CoreLibs/timer"
+
+-- Declaring this "gfx" shorthand will make your life easier. Instead of having
+-- to preface all graphics calls with "playdate.graphics", just use "gfx."
+-- Performance will be slightly enhanced, too.
+-- NOTE: Because it's local, you'll have to do it in every .lua source file.
+
+local gfx <const> = playdate.graphics
+
+
+
+function calculator_setup()
+
+  gfx.drawText("PlayDate Calculator", 10, 10)
+
+end
+
+-- Now we'll call the function above to configure our game.
+-- After this runs (it just runs once), nearly everything will be
+-- controlled by the OS calling `playdate.update()` 30 times a second.
+
+calculator_setup()
+
+-- `playdate.update()` is the heart of every Playdate game.
+-- This function is called right before every frame is drawn onscreen.
+-- Use this function to poll input, run game logic, and move sprites.
+
+function playdate.update()
+
+  playdate.timer.updateTimers()
+
+end
